7007648cca20e7247ed1fad53d775d8208252a40
Nicholas Martiny Roberts <nickmartinyroberts@gmail.com>
2016-09-15 13:34:01 +0100

    Add files via upload
    final 'attempt'

--- Memory-Game/main.js
+++ Memory-Game/main.js
@@ -1,28 +1,60 @@
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'King';
-var cardFour = 'King';
+        var cardOne = document.getElementById('cardOne').onclick = function() {
+			document.getElementById('cardOne').innerHTML = '<img src="/static/uploads/photo/2015/08/11/11/57/spades-884203_960_720.png">'
+		}
+		var cardTwo = document.getElementById('cardTwo').onclick = function() {
+			document.getElementById('cardTwo').innerHTML = '<img src="/static/uploads/photo/2015/08/11/11/57/spades-884203_960_720.png>'}
+		var cardThree = document.getElementById('cardThree').onclick = function() {
+			document.getElementById('cardThree').innerHTML = '<img src="/static/uploads/photo/2015/08/11/11/57/diamonds-884195_960_720.png">'}	
+		var cardFour = document.getElementById('cardFour').onclick = function() {
+			document.getElementById('cardFour').innerHTML = <img src="/static/uploads/photo/2015/08/11/11/57/diamonds-884195_960_720.png>'
+		}
 
-// if (cardOne === cardTwo){
-// 	alert ("You found a match!");
-// } else if (cardThree === cardFour){
-// 	alert ("You found a match!");
-// } else {
-// 	alert ("Sorry, try again.")
-// }
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var gameboard = document.getElementById("game-board")
 
 var createBoard = function(){
 	var oldBoard = document.getElementById("game-board");
-	for(var i = 0; i < 4; i++) {
+	for(var i = 0; i < cards.length; i++) {
       var newCards = document.createElement('div');
       newCards.className = "card";
       oldBoard.appendChild(newCards);
+      cardElement.setAttribute('data-card', cards[i]);
+      cardElement.addEventListener('click', isTwoCards);
+
 	}
 };
+
+var isMatch = function (){  
+if (cards[0] === cards[1]) {
+	alert ("You found a match!");
+} else if (cards[2] === cards[3]){
+	alert ("You found a match!");
+} else {
+	alert ("Sorry, try again.")
+}
+this.innerHTML = "none";
+};
+
+var isTwoCards = function (){
+  cardsInPlay.push(this.getAttribute('data-card'));
+   if (cardsInPlay.length === 2) {
+   isMatch(cardsInPlay);
+   cardsInPlay = [];
+}
+}
+
+
+if (this.getAttribute('data-card') === 'king' || this.getAttribute('data-card') === 'king') {
+this.innerHTML = '<img src="/static/uploads/photo/2015/08/11/11/57/diamonds-884195_960_720.png">'
+} else if (this.getAttribute('data-card') === 'queen' || this.getAttribute('data-card') === 'queen') {
+this.innerHTML = '<img src="/static/uploads/photo/2015/08/11/11/57/spades-884203_960_720.png">'}
+};
+
+
 createBoard();
 
-// var createBoard = function(){
-// 	for(i = 0); i<4; i++){
-// 	gameBoard.appendChild(createBoard);
-// }
-// }
+
+
+
